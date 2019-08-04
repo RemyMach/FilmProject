3b4f3280be2ebb68fc069e814dd35d7b83e4f004
remy <remygt@hotmail.fr>
2019-08-04 12:57:27 +0200

    ajout du bouton dans le header, rendu au format android(header du prochain commit en ios)

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -8,12 +8,31 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 class FilmDetail extends React.Component {
 
+    static navigationOptions = ({ navigation }) => {
+        const {params} = navigation.state
+        //On accède à la fonction shareFilm et au film via les paramètres qu'on a ajouté à la navigation
+        //normalemet c'est pour ios là pour ce commmit j'ai mit android et j'ai adapté au prochain ce sera ios
+        if (params.film != undefined && Platform.OS === 'android'){
+            return {
+                //on a besoin d'afficher une image, il faut donc passe par une Touchabe une fois
+                headerRight: <TouchableOpacity 
+                                style={styles.share_tochable_headerrightbutton}
+                                onPress={() => params.shareFilm()} >
+                                <Image
+                                    style={styles.share_image}
+                                    source={require('../Images/ic_share.png')} />
+                            </TouchableOpacity>
+            }
+        }
+    }
+
     constructor(props){
         super(props);
         this.state = {
             film: undefined, // Pour l'instant on n'a pas les infos du film, on initialise donc le film à undefined.
             isLoading: false // A l'ouverture de la vue, on affiche le chargement, le temps de récupérer le détail du film
         }
+        this._shareFilm = this._shareFilm.bind(this)
     }
 
     _displayLoading() {
@@ -74,13 +93,23 @@ class FilmDetail extends React.Component {
         }
     }
 
+    // Fonction pour faire passer la fonction _shareFilm et le film aux paramètres de la navigation. Ainsi on aura accès à ces données au moment de définir le headerRight
+    _updateNavigationParams() {
+        this.props.navigation.setParams({
+            shareFilm: this._shareFilm,
+            film: this.state.film
+        })
+    }
+
+    // Dès que le film est chargé, on met à jour les paramètres de la navigation (avec la fonction _updateNavigationParams) pour afficher le bouton de partage
     componentDidMount() {
         const favoriteFilmIndex = this.props.favoritesFilm.findIndex(item => item.id === this.props.navigation.state.params.idFilm)
         if (favoriteFilmIndex !== -1) { // Film déjà dans nos favoris, on a déjà son détail
             // Pas besoin d'appeler l'API ici, on ajoute le détail stocké dans notre state global au state de notre component
             this.setState({
-              film: this.props.favoritesFilm[favoriteFilmIndex]
-            })
+              film: this.props.favoritesFilm[favoriteFilmIndex]},
+              () => {this._updateNavigationParams()}
+            )
             return
           }else{
             // Le film n'est pas dans nos favoris, on n'a pas son détail
@@ -89,8 +118,9 @@ class FilmDetail extends React.Component {
             getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
                 this.setState({
                     film: data,
-                    isLoading: false
-                })
+                    isLoading: false},
+                    () => {this._updateNavigationParams()}
+                )
             })
         }
     }
@@ -239,6 +269,15 @@ const styles = StyleSheet.create({
     share_image: {
         width: 30,
         height: 30
+    },
+    share_tochable_headerrightbutton: {
+        width: 50,
+        height: 50,
+        margin: 8,
+        backgroundColor: '#e91e63',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 30
     }
 })
 
