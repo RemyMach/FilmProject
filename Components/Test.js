8832935d02120134efeb4187e601f32d7f5aec26
remy <remygt@hotmail.fr>
2019-08-06 21:02:56 +0200

    exemple de l'utilistaion de la librairie Animated

--- Components/Test.js
+++ Components/Test.js
@@ -1,17 +1,48 @@
 // Components/Test.js
 
 import React from 'react'
-import { StyleSheet, View, Platform } from 'react-native'
-import HelloWorld from './HelloWorld'
-class Test extends React.Component {
+import { StyleSheet, View, Platform, Animated, Easing } from 'react-native'
 
-  render() {
-    return (
-      <View style={styles.main_container}>
-        <HelloWorld/>
-      </View>
-    )
-  }
+class Test extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            //on définit l'animated value dans le state
+            topPosition: new Animated.Value(0),
+            leftPosition: new Animated.Value(0)
+        }
+    }
+    //appelé quand notre component est monté
+    componentDidMount() {
+        Animated.parallel([
+            Animated.spring(
+              this.state.topPosition,
+              {
+                toValue: 100,
+                tension: 8,
+                friction: 3
+              }
+            ),
+            Animated.timing(
+              this.state.leftPosition,
+              {
+                toValue: 100,
+                duration: 1000,
+                easing: Easing.elastic(2)
+              }
+            )
+          ]).start()
+    }
+
+    render() {
+        //Animated.View est obligatoire pour faire une animation avec Animated
+        return (
+        <View style={styles.main_container}>
+            <Animated.View style={[styles.animation_view, {top: this.state.topPosition, left: this.state.leftPosition}]}>
+            </Animated.View>
+        </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -31,6 +62,11 @@ const styles = StyleSheet.create({
         }),
         height: 50,
         width: 50
+    },
+    animation_view: {
+        backgroundColor: 'red',
+        width: 100,
+        height: 100
     }
 })
 
